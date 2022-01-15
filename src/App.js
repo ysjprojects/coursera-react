f0d36f9283ce0f1dde3d54e72cfd7394fbab1cb1
ysjprojects <yshijie1999@gmail.com>
2022-01-15 15:35:44 +0800

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Navbar } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar dark color="primary">
+          <div className="container">
+            <navbarBrand href="/">
+              Ristorante Con Fusion
+            </navbarBrand>
+          </div>
+        </Navbar>
+
       </div>
     );
   }
